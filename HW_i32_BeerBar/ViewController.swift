c80f3b1efa95081164a7fdbfb128443abe231463
Антон Кулик <antonio5398883@gmail.com>
2023-06-18 00:07:13 +0300

    Initial commit

---
+++ HW_i32_BeerBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_i32_BeerBar
+//
+//  Created by Антон Кулик on 13.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
